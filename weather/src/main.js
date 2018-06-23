27ae99ab5046e244dab12b573a746dde2bdd43a0
kdnakt <a.kid.1985@gmail.com>
2018-06-23 20:59:29 +0900

    weather: remove unused data

--- weather/src/main.js
+++ weather/src/main.js
@@ -4,14 +4,5 @@ import CityList from './components/CityList.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  data: function() {return {
-    cities: [{
-      id: 1,
-      name: "Tokyo"
-    }, {
-      id: 2,
-      name: "London"
-    }]
-  }},
   render: h => h(CityList)
 }).$mount('#app')
