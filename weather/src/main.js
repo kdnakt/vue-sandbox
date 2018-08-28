35fcba10889968031f0be3825a5409a0ba4625a8
kdnakt <a.kid.1985@gmail.com>
2018-08-28 09:49:18 +0900

    weather: load and store forecasts

--- weather/src/main.js
+++ weather/src/main.js
@@ -31,10 +31,26 @@ async function getWeatherForecast(apikey, name) {
     json.list.forEach(fc => {
       forecasts.push(fc)
     })
-    return forecasts
+    return { cod: json.cod, forecasts: forecasts }
   } catch (e) {
     return e
   }
+}
+
+async function getWeather(apikey, name) {
+  const current = await getCurrentWeather(apikey, name)
+  if (current.cod == 200) {
+    store.commit('weather/setCurrent', current)
+  } else {
+    return current
+  }
+  const forecasts = await getWeatherForecast(apikey, name)
+  if (forecasts.cod == 200) {
+    store.commit('weather/setForecasts', forecasts.forecasts)
+  } else {
+    return forecasts
+  }
+  return { cod: 200 }
 }
 
 const router = new VueRouter({
@@ -50,9 +66,8 @@ const router = new VueRouter({
           next("/")
           return
         }
-        getCurrentWeather(apikey, to.params.name).then(data => {
+        getWeather(apikey, to.params.name).then(data => {
           if (data.cod === 200) {
-            store.commit('weather/setCurrent', data)
             next()
           } else {
             alert("Something went wrong! " + data.message)
